71df872ae4864f9ab5fa454773ffa56a20d8aac2
Luis Torres <luis.t122@gmail.com>
2024-10-17 12:44:45 -0400

    remove pycaches

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from src.core.db import create_db_and_tables
 from src.api.main import api_router
 from src.models import Todo, TodoCreate, TodoPublic, TodosPublic
 
-api_version="/api/v1"
+api_version="/v1"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,5 +17,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# Separate API routes from base path
+api = FastAPI()
+api.include_router(api_router, prefix=api_version)
 
-app.include_router(api_router, prefix=api_version)
+app.mount("/api", api)
